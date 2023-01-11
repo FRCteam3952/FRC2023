b82a2351e400be9240b18ba1c6b26a7bf41739ec
Sean Son <astroson2005@gmail.com>
2023-01-11 13:50:18 -0800

    code I wrote during Koci because I can

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,6 +31,12 @@ public final class Constants {
     public static final double STARTING_Z = 0;
   }
 
+  public static class ClawConstants{
+
+    public static final double CLAW_ROTATE_SPEED = 0.1;
+    public static final double ANGLE_DELTA = 2;
+  }
+
   public static class OperatorConstants {
     public static final int RIGHT_JOYSTICK_PORT = 0;
     public static final int LEFT_JOYSTICK_PORT = 1;
--- src/main/java/frc/robot/commands/ClawCommands/ClawRotateCommand.java
+++ src/main/java/frc/robot/commands/ClawCommands/ClawRotateCommand.java
@@ -3,6 +3,8 @@ package frc.robot.commands.ClawCommands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ClawSubsystem;
 
+import frc.robot.Constants.ClawConstants;
+
 public class ClawRotateCommand extends CommandBase{
     private final ClawSubsystem clawSubsystem;
     private double angle;
@@ -19,16 +21,23 @@ public class ClawRotateCommand extends CommandBase{
     
     @Override
     public void execute(){
+        if(clawSubsystem.getClawAngle() < angle){
+            clawSubsystem.setClawRotateSpeed(ClawConstants.CLAW_ROTATE_SPEED);
+        }
+        else{
+            clawSubsystem.setClawRotateSpeed(-ClawConstants.CLAW_ROTATE_SPEED);;
+        }
 
+        //max do the PID thingy
     }
 
     @Override
     public void end(boolean interrupted) {
-
+        clawSubsystem.setClawRotateSpeed(0);
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return Math.abs(angle - clawSubsystem.getClawAngle()) < ClawConstants.ANGLE_DELTA;
     }
 }
