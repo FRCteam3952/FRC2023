bc65a955fc6c44e00d3f9c9e296689aee2f3aca9
LFC6times <72238524+LFC6times@users.noreply.github.com>
2023-01-18 17:38:16 -0800

    Reorganized code to split ClawSubsystem into a ClawGrip and a ClawRotate for better individual operation.

---
+++ src/main/java/frc/robot/subsystems/ClawGripSubsystem.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants.PortConstants;
+
+public class ClawGripSubsystem extends SubsystemBase {
+    private final CANSparkMax clawGrip;
+    
+    private final RelativeEncoder clawGripEncoder;
+
+    private boolean clawState;
+
+    public ClawGripSubsystem() {
+        this.clawGrip = new CANSparkMax(PortConstants.CLAW_GRIP_PORT, MotorType.kBrushless);
+
+        this.clawGripEncoder = this.clawGrip.getEncoder();
+
+        this.clawState = false;
+    }
+
+    public void openClaw(){
+        // TODO: IMPLEMENT
+    }
+
+    public void closeClaw(){
+        // TODO: IMPLEMENT
+    }
+
+    public boolean getClawState(){
+        return this.clawState; // rename variable to more descriptive?
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        
+    }
+}
