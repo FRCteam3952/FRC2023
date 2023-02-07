48c2dde42d48e09f92a3a6f2b5e333d505706d94
Sean Son <astroson2005@gmail.com>
2023-02-07 10:07:05 -0800

    ayo

--- src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -1,13 +1,10 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.NetworkTable;
+import frc.robot.wrappers.NetworkTables;
 import edu.wpi.first.math.controller.PIDController;
 
 public class LimeLightSubsystem extends SubsystemBase {
-    private final NetworkTableInstance inst;
-    private final NetworkTable table;
     private final PIDController pidcontrol;
     private final float kp = 0.003125f;
     private final float ki = 0.01f;
@@ -17,13 +14,10 @@ public class LimeLightSubsystem extends SubsystemBase {
 
     public LimeLightSubsystem() {
         pidcontrol = new PIDController(1, ki, kd);
-        inst = NetworkTableInstance.getDefault();
-        table = inst.getTable("limelight"); // initiate limelight table
     }
 
     public double getAdjustment() {
-        float info[] = table.getEntry("llpython").getFloatArray(new float[3]);
-        float tx = (info[1] - 160) * kp;
+        float tx = (NetworkTables.getLimeLightError() - 160) * kp;
 
         if(tx > 1){
             tx =1;
@@ -39,16 +33,7 @@ public class LimeLightSubsystem extends SubsystemBase {
         }
         double steering_adjust = pidcontrol.calculate(tx) * -1; // calculate PID
         
-        return steering_adjust;
-    }
-
-    public void turnOnLED() {
-        table.getEntry("ledMode").setDouble(3);
-    }
-
-    
-    public void turnOffLED() {
-        table.getEntry("ledMode").setDouble(1);
+        return steering_adjust; 
     }
 
     @Override
