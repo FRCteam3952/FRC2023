93c8643bcd4b7c1e80c8b29de7058bda35d7645b
Barbara Cao <800023833@fjuhsd.org>
2023-02-18 12:13:34 -0800

    AutoOne and AutoTwo is filler code (delete later)

--- src/main/java/frc/robot/commands/AutoCommands/AutoOne.java
+++ src/main/java/frc/robot/commands/AutoCommands/AutoOne.java
@@ -1,10 +1,16 @@
 package frc.robot.commands.AutoCommands;
 
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveTrainSubsystem;
+import frc.robot.subsystems.Gyro;
+import frc.robot.subsystems.LimeLightSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.DriveTrainSubsystem;
+import edu.wpi.first.wpilibj.Timer;
 
-public class AutoOne {
-    
+public class AutoOne extends CommandBase {
+    public static CommandBase exampleAuto() { //this is just a filler, will delete this later
+      return null;
+    }
 }
