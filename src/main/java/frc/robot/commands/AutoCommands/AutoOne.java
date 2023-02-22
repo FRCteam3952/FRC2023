cef98cbba094e63861474503b407b2e41888d10e
bcow25 <122178327+bcow25@users.noreply.github.com>
2023-02-21 16:29:55 -0800

    i think i added constructors???

--- src/main/java/frc/robot/commands/AutoCommands/AutoOne.java
+++ src/main/java/frc/robot/commands/AutoCommands/AutoOne.java
@@ -1,16 +1,14 @@
 package frc.robot.commands.AutoCommands;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.Gyro;
 import frc.robot.subsystems.LimeLightSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.ArmSubsystem;
-import edu.wpi.first.wpilibj.Timer;
 
-public class AutoOne extends CommandBase {
-    public static CommandBase exampleAuto() { //this is just a filler, will delete this later
-      return null;
-    }
+public class AutoOne {
+  public AutoOne() {
+
+  }
 }
