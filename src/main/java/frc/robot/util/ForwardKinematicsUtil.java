c0f1ec72461aab6fb52d52f33632137e02e83dc9
Sean Son <astroson2005@gmail.com>
2023-02-22 17:22:26 -0800

    arm stuff

--- src/main/java/frc/robot/util/ForwardKinematicsUtil.java
+++ src/main/java/frc/robot/util/ForwardKinematicsUtil.java
@@ -11,17 +11,10 @@ public final class ForwardKinematicsUtil {
      * a1, a2, turretAngle in degrees!!!
      */
     public static double[] getCoordinatesFromAngles(double a1, double a2, double turretAngle) {
-        double x = 0.0, y = ArmConstants.ORIGIN_HEIGHT, z = 0.0, dist = 0.0;
-        double angleSegment2 = Math.toRadians(90 + a1 - a2); 
-        double a1Rad = Math.toRadians(a1);
-        turretAngle = Math.toRadians(turretAngle);
+        double z = 0;
+        double y = -ArmConstants.LIMB1_LENGTH * Math.cos(Math.toRadians(a1)) + ArmConstants.LIMB2_LENGTH * Math.cos(Math.toRadians(a2-a1));
+        double x = ArmConstants.LIMB1_LENGTH * Math.sin(Math.toRadians(a1)) + ArmConstants.LIMB2_LENGTH * Math.sin(Math.toRadians(a2-a1));
 
-        //redid forward kinematics to match inverse kinematics
-        dist = ArmConstants.LIMB1_LENGTH * Math.sin(a1Rad) + ArmConstants.LIMB2_LENGTH * Math.cos(angleSegment2);
-        z = Math.sin(turretAngle) * dist;
-        x = Math.cos(turretAngle) * dist;
-        y += a1 > 90? ArmConstants.LIMB1_LENGTH * Math.cos(a1Rad) : ArmConstants.LIMB1_LENGTH * Math.cos(a1Rad) * -1;
-        y += ArmConstants.LIMB2_LENGTH * Math.sin(angleSegment2);
 
         // Solve for the distance from origin -> claw
         return new double[] {x, y, z};
--- src/main/java/frc/robot/util/InverseKinematicsUtil.java
+++ src/main/java/frc/robot/util/InverseKinematicsUtil.java
@@ -2,6 +2,7 @@ package frc.robot.util;
 
 import frc.robot.Constants.ArmConstants;
 
+
 public final class InverseKinematicsUtil {
     private static double x_pos, y_pos, z_pos;
 
@@ -12,44 +13,58 @@ public final class InverseKinematicsUtil {
     /**
      * calculate arm angles relative to limb that it's attached to
      */
-    public static double[] getAnglesFromCoordinates(double x, double y, double z) {
+    public static double[] getAnglesFromCoordinates(double x, double y, double z, boolean flipped) {
         double a1, a2, turretAngle;
         double adjusted_y = y - ArmConstants.ORIGIN_HEIGHT;   // calculate height relative to the origin (at the tip of the non-moving rod which holds the arm)
-        double adjusted_x = Math.abs(x);
+        double adjusted_x = x;
+        if (x < 5){
+            adjusted_x = 5;
+        }
         double adjusted_z = z;
         double dist3d = MathUtil.distance(0, adjusted_x, 0, adjusted_y, 0, z);     // calc distance in 3d from top pivot point
-        x_pos = x;
-        y_pos = y;
-        z_pos = z;
         double totalLimbLength = ArmConstants.LIMB1_LENGTH + ArmConstants.LIMB2_LENGTH;
-        if (dist3d >= totalLimbLength - ArmConstants.DISTANCE_DELTA) {
-            adjusted_x *= ((totalLimbLength - ArmConstants.DISTANCE_DELTA) / dist3d); 
-            adjusted_y *= ((totalLimbLength - ArmConstants.DISTANCE_DELTA) / dist3d);
-            adjusted_z *= ((totalLimbLength - ArmConstants.DISTANCE_DELTA) / dist3d); 
-            x_pos *= ((totalLimbLength - ArmConstants.DISTANCE_DELTA) / dist3d); 
-            y_pos = adjusted_y * ((totalLimbLength - ArmConstants.DISTANCE_DELTA) / dist3d) + ArmConstants.ORIGIN_HEIGHT;
-            z_pos *= ((totalLimbLength - ArmConstants.DISTANCE_DELTA) / dist3d);
+        if(dist3d > totalLimbLength) {
+            adjusted_x *= (totalLimbLength / dist3d); 
+            adjusted_y *= (totalLimbLength / dist3d);
+            adjusted_z *= (totalLimbLength / dist3d); 
+            x_pos = adjusted_x;
+            y_pos = adjusted_y;
+            z_pos = adjusted_z;
         }
-        if (dist3d <= ArmConstants.MIN_DISTANCE + ArmConstants.DISTANCE_DELTA) {
-            adjusted_x *= ((ArmConstants.MIN_DISTANCE + ArmConstants.DISTANCE_DELTA) / dist3d); 
-            adjusted_y *= ((ArmConstants.MIN_DISTANCE + ArmConstants.DISTANCE_DELTA) / dist3d);
-            adjusted_z *= ((ArmConstants.MIN_DISTANCE + ArmConstants.DISTANCE_DELTA) / dist3d); 
-            x_pos *= ((ArmConstants.MIN_DISTANCE + ArmConstants.DISTANCE_DELTA) / dist3d); 
-            y_pos = adjusted_y * ((ArmConstants.MIN_DISTANCE + ArmConstants.DISTANCE_DELTA) / dist3d) + ArmConstants.ORIGIN_HEIGHT;
-            z_pos *= ((ArmConstants.MIN_DISTANCE + ArmConstants.DISTANCE_DELTA) / dist3d);
-        }
-
 
         if (dist3d == 0) { //zero, zero on coordinate -> prevent divide by 0 exception
             return new double[] {0,0,0};
         }           
-        a2 = MathUtil.lawOfCosinesForAngle(ArmConstants.LIMB1_LENGTH, ArmConstants.LIMB2_LENGTH, dist3d);                                  // a2 is angle between 1st arm segment to 2nd arm segment
+        a2 = MathUtil.lawOfCosinesForAngle(ArmConstants.LIMB1_LENGTH, ArmConstants.LIMB2_LENGTH, dist3d); // a2 is angle between 1st arm segment to 2nd arm segment
         a1 = MathUtil.angleBetweenLines(0, -1, 0, adjusted_x, adjusted_y, adjusted_z) - MathUtil.lawOfSinesForAngle(a2, dist3d, ArmConstants.LIMB2_LENGTH);   // a1 is angle between verticle to 1st arm segment
        
+        //if flipped is true, return angles that are "flipped" 
+        if(flipped){
+            double angleCalc = Math.toDegrees(Math.atan2(adjusted_x, -y + ArmConstants.ORIGIN_HEIGHT));
+            double lineAngle = angleCalc < 0 ? 360 + angleCalc : angleCalc;
+            a1 = lineAngle*2 - a1;
+            a2 = 360 - a2;
+            if (a1 > 350){
+                a1 = 350;
+            }
+            if(a2 > 345){
+                a2 = 345;
+            }
+        }
+        else{
+            if (a1 < 10){
+                a1 = 10;
+            }
+            if(a2 < 15){
+                a2 = 15;
+            }
+        }
+        
+
         //turret angle calculations
         double angleCalc = Math.toDegrees(Math.atan2(z, x));
         turretAngle = angleCalc < 0 ? 360 + angleCalc : angleCalc;
-        
+
         return new double[] {a1, a2, turretAngle};
     }
 
