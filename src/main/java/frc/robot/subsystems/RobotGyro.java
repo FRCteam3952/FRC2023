16736d897f2f7407d646adf404b121e2a500e422
Andy <24chenay@gmail.com>
2023-02-23 21:58:12 -0800

    Calibrate RoboGyro for calculating Drift

--- src/main/java/frc/robot/subsystems/RobotGyro.java
+++ src/main/java/frc/robot/subsystems/RobotGyro.java
@@ -9,6 +9,10 @@ import edu.wpi.first.wpilibj.ADIS16470_IMU;
 
 public class RobotGyro extends SubsystemBase {
     private static final ADIS16470_IMU gyro = new ADIS16470_IMU();
+    private static final int DRIFT_MEASUREMENT_SAMPLES = 20; //specifies the number of measurements to take during each interval CHANGED IF NEEDED
+    private static final int DRIFT_MEASUREMENT_INTERVAL_MS = 25; //specifies the duration of each measurement interval in milliseconds CHANGE IF NEEDED
+    private static double drift = 0.0;
+    
 
     static {
         gyro.setYawAxis(ADIS16470_IMU.IMUAxis.kZ);
@@ -45,10 +49,27 @@ public class RobotGyro extends SubsystemBase {
         gyro.calibrate();
     }
 
+    private static void measureDrift() {
+        double startAngle = gyro.getAngle();
+        double sumAngle = 0.0;
+        long startTime = System.currentTimeMillis();
+    
+        for (int i = 0; i < DRIFT_MEASUREMENT_SAMPLES; i++) {
+            while (System.currentTimeMillis() < startTime + DRIFT_MEASUREMENT_INTERVAL_MS) {
+                // wait until the measurement interval has elapsed
+            }
+            sumAngle += gyro.getAngle() - startAngle;
+            startTime += DRIFT_MEASUREMENT_INTERVAL_MS;
+        }
+    
+        double driftRate = sumAngle / DRIFT_MEASUREMENT_INTERVAL_MS / DRIFT_MEASUREMENT_SAMPLES; // the average change in angle per second during each measurement interval
+        drift = drift + driftRate * DRIFT_MEASUREMENT_INTERVAL_MS * DRIFT_MEASUREMENT_SAMPLES / 1000.0;// adds the driftrate *duration of each measurement * num samples and converts to seconds
+    }
     @Override
     public void periodic() {
         // System.out.println(getGyroAngle());
         // resetGyroAngle();
+        measureDrift();
     }
 
     @Override
