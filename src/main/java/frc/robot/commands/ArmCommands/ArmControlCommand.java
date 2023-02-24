8824f0cc1e783d13ce52f58fee8fcb992350ae8b
Derek <derekyp9@gmail.com>
2023-02-23 23:03:58 -0800

    two functions implemented, joystick constants fix

--- src/main/java/frc/robot/commands/ArmCommands/ArmControlCommand.java
+++ src/main/java/frc/robot/commands/ArmCommands/ArmControlCommand.java
@@ -10,12 +10,12 @@ import frc.robot.subsystems.LimeLightSubsystem;
 /**
  * Moves arm on the turret
  */
-public class ArmControlCommand extends CommandBase {
+public class ArmControlCommand extends CommandBase{
     private final ArmSubsystem arm;
     private final FlightJoystick joystick;
     private final double areaConst;
     private final double xSpeed, ySpeed, zSpeed, turretSpeed;
-    private static final double DESIRED_AREA = 369; // in pixels probably, can tune later
+    private final double DESIRED_AREA = 369; // in pixels probably, can tune later
 
     public ArmControlCommand(ArmSubsystem arm, FlightJoystick joystick) {
         this.arm = arm;
@@ -32,44 +32,46 @@ public class ArmControlCommand extends CommandBase {
 
     // Gets adjustments from limelight and converts them to position adjustments
     private double[] getAdjustmentFromError() {
-        double[] adjustments = new double[4];
-        adjustments[0] = Math.sin(arm.getCurrentAnglesRad()[2]) * (DESIRED_AREA - LimeLightSubsystem.getArea()) / areaConst; // x-axis adjustment
+        double[] adjustments = new double[3];
+        adjustments[0] = Math.sin(arm.getCurrentAnglesRad()[2]) * LimeLightSubsystem.getXAdjustment() 
+            + Math.cos(arm.getCurrentAnglesRad()[2]) * (DESIRED_AREA - LimeLightSubsystem.getArea())/areaConst; // x-axis adjustment
 
         adjustments[1] = LimeLightSubsystem.getYAdjustment(); // y-axis adjustment
 
-        adjustments[2] = Math.cos(arm.getCurrentAnglesRad()[2]) * (DESIRED_AREA - LimeLightSubsystem.getArea() / areaConst); // z-axis adjustment
-
-        adjustments[3] = LimeLightSubsystem.getXAdjustment(); // turret angle adjustment
+        adjustments[2] = Math.cos(arm.getCurrentAnglesRad()[2]) * LimeLightSubsystem.getXAdjustment() 
+            + Math.sin(arm.getCurrentAnglesRad()[2]) * (DESIRED_AREA - LimeLightSubsystem.getArea()/areaConst); // z-axis adjustment
 
         return adjustments;
-    }
+}
 
     // Primary arm control
     private void primaryArmControl() {
         if (joystick.getRawButtonWrapper(ControllerConstants.AIM_ASSIST_BUTTON_NUMBER)) { // Aim assist
-            double[] adjustments = this.getAdjustmentFromError();
-            arm.setTurretSpeed(adjustments[3] * turretSpeed);
-            arm.moveVector(adjustments[0] * xSpeed, adjustments[1] * ySpeed, adjustments[2] * zSpeed);
+            arm.moveVector(getAdjustmentFromError()[0] * xSpeed, getAdjustmentFromError()[1] * ySpeed, getAdjustmentFromError()[2] * zSpeed);
         } else {
             double y = 0;
             if (joystick.getRawButtonWrapper(ControllerConstants.MOVE_ARM_UP_BUTTON_NUMBER)) {
-                y = ySpeed;
+                y =  ySpeed;
             } else if (joystick.getRawButtonWrapper(ControllerConstants.MOVE_ARM_DOWN_BUTTON_NUMBER)) {
                 y = -ySpeed;
-            }
+            } 
             arm.setTurretSpeed(joystick.getHorizontalMovement() * turretSpeed);
-            double turretAngleRad = arm.getCurrentAnglesRad()[2];
-            arm.moveVector(Math.sin(turretAngleRad) * joystick.getLateralMovement() * xSpeed, // Handles extension of robot arm 
-                    y,
-                    Math.cos(turretAngleRad) * joystick.getLateralMovement() * zSpeed);
+            arm.moveVector(Math.sin(arm.getCurrentAnglesRad()[2]) * joystick.getLateralMovement() * xSpeed, // Handles extension of robot arm 
+                y, 
+                Math.cos(arm.getCurrentAnglesRad()[2]) * joystick.getLateralMovement() * zSpeed);
         }
     }
 
     // Moves arm to preset distance above the floor for picking up gamepieces 
-    private void pickUpPosition() {
-        if (joystick.getRawButtonWrapper(ControllerConstants.MOVE_ARM_TO_PICK_UP_POSITION_BUTTON_NUMBER)) {
-            double[] armCoordinates = arm.getCurrentCoordinates();
-            arm.setIntendedCoordinates(armCoordinates[0], ArmConstants.PICK_UP_POSITION_Y, armCoordinates[2]);
+    private void pickUpPositionFlipped() {
+        if (joystick.getRawButtonWrapper(ControllerConstants.MOVE_ARM_TO_PICK_UP_POSITION_BUTTON_NUMBER_FLIPPED)) {
+            arm.setIntendedCoordinates(arm.getCurrentCoordinates()[0], ArmConstants.PICK_UP_POSITION_Y, arm.getCurrentCoordinates()[2]);
+        }
+    }
+
+    private void pickUpPositionNotFlipped() {
+        if (joystick.getRawButtonWrapper(ControllerConstants.MOVE_ARM_TO_PICK_UP_POSITION_BUTTON_NUMBER_NOT_FLIPPED)) {
+            arm.setIntendedCoordinates(arm.getCurrentCoordinates()[0], ArmConstants.PICK_UP_POSITION_Y, arm.getCurrentCoordinates()[2]);
         }
     }
 
@@ -86,7 +88,6 @@ public class ArmControlCommand extends CommandBase {
         }
     }
 
-
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
@@ -96,6 +97,8 @@ public class ArmControlCommand extends CommandBase {
     @Override
     public void execute() {
         // testPrimaryArmControl();
+        pickUpPositionFlipped();
+        pickUpPositionNotFlipped();
         primaryArmControl();
         // pickUpPosition();
         togglePIDControl();
@@ -103,8 +106,7 @@ public class ArmControlCommand extends CommandBase {
 
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {
-    }
+    public void end(boolean interrupted) {}
 
     // Returns true when the command should end.
     @Override
