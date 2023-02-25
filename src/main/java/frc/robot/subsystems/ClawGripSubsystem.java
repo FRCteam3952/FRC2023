0da5c490df6a4691d8d62a9c300fbb72231591d3
Andy <24chenay@gmail.com>
2023-02-25 13:10:16 -0800

    Add Claw Limit Switch

--- src/main/java/frc/robot/subsystems/ClawGripSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawGripSubsystem.java
@@ -6,21 +6,25 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 import frc.robot.Constants.PortConstants;
 import frc.robot.Constants.ClawConstants;
+import frc.robot.Constants.ControllerConstants;
 
 public class ClawGripSubsystem extends SubsystemBase {
     private final CANSparkMax clawGrip;
     private final RelativeEncoder clawGripEncoder;
     private boolean clawState;
-
+    private final DigitalInput claw1Limit;
     public ClawGripSubsystem() {
         this.clawGrip = new CANSparkMax(PortConstants.CLAW_GRIP_PORT, MotorType.kBrushless);
         this.clawGripEncoder = this.clawGrip.getEncoder();
         this.clawState = false;
-    }
+        this.claw1Limit = new DigitalInput(ControllerConstants.CLAW_GRIP_BUTTON_NUMBER);//Change Constant Value
 
+    }
+  
     // Runs once when claw grip trigger is released
     public CommandBase openClaw() {
         return this.runOnce(
@@ -43,6 +47,10 @@ public class ClawGripSubsystem extends SubsystemBase {
                     }
                 });
     }
+    public boolean getClawLimitPressed() {
+        return !this.claw1Limit.get();
+    }
+   
 
     public boolean getClawState() {
         return this.clawState; // rename variable to more descriptive?
@@ -50,7 +58,10 @@ public class ClawGripSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-
+        if(getClawLimitPressed()){
+            clawGrip.set(0.0);
+            clawGripEncoder.setPosition(ClawConstants.MIN_GRIP_ENCODER_VALUE);
+        }
     }
 
     @Override
