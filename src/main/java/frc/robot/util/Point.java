c5c3c780ee31c7a8e036ddba59d86b08a9f7c98c
Ivan Zimmerman <800025142@fjuhsd.org>
2023-03-04 13:56:50 -0800

    added point class to possibly use in future

---
+++ src/main/java/frc/robot/util/Point.java
@@ -0,0 +1,64 @@
+package frc.robot.util;
+
+import java.util.function.BiFunction;
+
+public class Point {
+    private double x, y, z;
+
+    public Point(double x, double y, double z) 
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public Point(double[] positions)
+    {
+        this.x = positions[0];
+        this.y = positions[1];
+        this.z = positions[2];
+    }
+    public Point()
+    {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+
+    public Point fromAngles(double x_angle, double y_angle, double length) {
+        throw new Error(); // todo
+    }
+    public Point fromXZY(double x, double z, double y) { return new Point(x, y, z); }
+    public Point fromXZYList(double[] x) { 
+        return fromXZY(x[0], x[1], x[2]); 
+    }
+
+    public double[] getCoordinates() {
+        return new double[]{
+            this.x,
+            this.y,
+            this.z,
+        };
+    }
+
+    public Point combineFields(Point other, BiFunction<Double, Double, Double> combine_func)
+    {
+        return new Point(
+            combine_func.apply(this.x, other.getX()),
+            combine_func.apply(this.y, other.getY()),
+            combine_func.apply(this.z, other.getZ())
+        );
+    }
+
+    public double getX() { return this.x; }
+    public double getY() { return this.y; }
+    public double getZ() { return this.z; }
+
+    public Point add(Point other) { return combineFields( other, (a, b) -> a + b );          }
+    public Point sub(Point other) { return combineFields( other, (a, b) -> a - b );          }
+    public Point mul(Point other) { return combineFields( other, (a, b) -> a * b );          }
+    public Point div(Point other) { return combineFields( other, (a, b) -> a / b );          }
+    public Point mod(Point other) { return combineFields( other, (a, b) -> a % b );          }
+    public Point pow(Point other) { return combineFields( other, (a, b) -> Math.pow(a,b) );  }
+    
+
+}
