db856bff298f4f6a6859e19c7c6a8eebde24a9b8
Sean Son <astroson2005@gmail.com>
2023-03-14 11:46:35 -0700

    update poseaimarmcommand to work with any 3d translation

--- src/main/java/frc/robot/commands/armcommands/PoseAimArmCommand.java
+++ src/main/java/frc/robot/commands/armcommands/PoseAimArmCommand.java
@@ -1,25 +1,26 @@
 package frc.robot.commands.armcommands;
-
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.PositionConstants;
+//import frc.robot.Constants.PositionConstants;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveTrainSubsystem;
-import frc.robot.util.NetworkTablesUtil;
+//import frc.robot.util.NetworkTablesUtil;
 
 public class PoseAimArmCommand extends CommandBase{
     private final ArmSubsystem arm;
     private final DriveTrainSubsystem drive;
-    private double[] coordinates;
+    private Translation3d armPose;
 
-    public PoseAimArmCommand(ArmSubsystem arm, DriveTrainSubsystem drive){
+    public PoseAimArmCommand(ArmSubsystem arm, DriveTrainSubsystem drive, Translation3d armPose){
         this.arm = arm;
         this.drive = drive;
+        this.armPose = armPose;
     }
 
     @Override
     public void initialize() {
-        int targetPoseID = NetworkTablesUtil.getKeyInteger();
+        /*int targetPoseID = NetworkTablesUtil.getKeyInteger();
         switch(targetPoseID){
             case 1:
                 coordinates = PositionConstants.BOTTOM_LEFT_POS;
@@ -51,16 +52,15 @@ public class PoseAimArmCommand extends CommandBase{
             default:
                 System.out.println("A key within 1-9 was not pressed");
                 break;
-        }
-
+        }*/
     }
 
     @Override
     public void execute() {
         Pose2d currentPose = drive.getPoseInches();
-        double targetX = coordinates[0] - currentPose.getX();
-        double targetZ = coordinates[2] - currentPose.getY();
-        double targetY = coordinates[1];
+        double targetX = armPose.getX() - currentPose.getX();
+        double targetZ = armPose.getZ() - currentPose.getY();
+        double targetY = armPose.getY();
         arm.setTargetCoordinates(targetX, targetY, targetZ);
     }
 
